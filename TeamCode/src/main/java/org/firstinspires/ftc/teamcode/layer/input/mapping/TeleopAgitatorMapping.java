4b3b352c166ebef06ac86783f99df19909b1f36f
plantainbread <zyuanboost2026@gmail.com>
2025-10-31 21:22:18 +0100

    Split IntakeTeleopMapping into two, combine IntakeLayer with ShooterLayer

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/layer/input/mapping/TeleopAgitatorMapping.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/layer/input/mapping/TeleopAgitatorMapping.java
@@ -5,10 +5,10 @@ import org.firstinspires.ftc.teamcode.layer.LayerSetupInfo;
 import org.firstinspires.ftc.teamcode.task.GamepadInputTask;
 import org.firstinspires.ftc.teamcode.task.Task;
 import org.firstinspires.ftc.teamcode.task.UnsupportedTaskException;
-import org.firstinspires.ftc.teamcode.task.IntakeTeleopTask;
+import org.firstinspires.ftc.teamcode.task.TeleopAgitatorTask;
 
-public final class IntakeTeleopMapping extends AbstractFunctionLayer {
-    public IntakeTeleopMapping() { }
+public final class TeleopAgitatorMapping extends AbstractFunctionLayer {
+    public TeleopAgitatorMapping() { }
 
     @Override
     public void setup(LayerSetupInfo setupInfo) { }
@@ -17,8 +17,7 @@ public final class IntakeTeleopMapping extends AbstractFunctionLayer {
     public Task map(Task task) {
         if (task instanceof GamepadInputTask) {
             GamepadInputTask castedTask = (GamepadInputTask) task;
-            return new IntakeTeleopTask(castedTask.gamepad0.dpad.left, castedTask.gamepad0.dpad.right,
-            castedTask.gamepad0.dpad.up, castedTask.gamepad0.dpad.down);
+            return new TeleopAgitatorTask(castedTask.gamepad0.dpad.left, castedTask.gamepad0.dpad.right);
         } else {
             throw new UnsupportedTaskException(this, task);
         }
