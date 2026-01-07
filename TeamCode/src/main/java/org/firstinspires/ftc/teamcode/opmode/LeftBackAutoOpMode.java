c1a504c1ae90e723f6f19352f2fd12282bb581e1
plantainbread <zyuanboost2026@gmail.com>
2026-01-07 15:14:14 -0800

    implement LeftBackAutoOpMode

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/LeftBackAutoOpMode.java
@@ -0,0 +1,34 @@
+package org.firstinspires.ftc.teamcode.opmode;
+
+import java.util.Arrays;
+import java.util.List;
+
+import com.qualcomm.robotcore.eventloop.opmode.autonomous;
+
+import org.firstinspires.ftc.teamcode.layer.Layer;
+import org.firstinspires.ftc.teamcode.layer.MultiplexLayer;
+import org.firstinspires.ftc.teamcode.layer.TopLayerSequence;
+import org.firstinspires.ftc.teamcode.layer.WinLayer;
+import org.firstinspires.ftc.teamcode.layer.drive.MecanumDrive;
+import org.firstinspires.ftc.teamcode.layer.ShooterIntakeLayer;
+import org.firstinspires.ftc.teamcode.layer.autonomous.LeftBackAuto;
+
+@Autonomous(name="Left Back Auto")
+public final class LeftBackAutoOpMode extends AbstractLayerOpMode {
+
+	public LeftBackAutoOpMode(){ }
+
+	@Override
+	protected List<Layer> getLayers() {
+		return Arrays.asList(
+			new MultiplexLayer(Arrays.asList(
+				new MecanumDrive(),
+				new ShooterIntakeLayer()
+			)),
+			new LeftBackAuto,
+			new TopLayerSequence(Arrays.asList(
+				new WinLayer()
+			))
+		);
+	}
+}
